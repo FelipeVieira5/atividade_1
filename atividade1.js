ce277f926206389ad1c93bd5a3b05d4c9e2ab3be
FelipeVieira5 <160546921+FelipeVieira5@users.noreply.github.com>
2024-02-21 18:21:33 -0300

    atividade 1 e 2

---
+++ atividade1.js
@@ -0,0 +1,9 @@
+function main(num) {
+    // codigo
+    if(num > 0){
+        return "positivo";
+    }else if(num < 0){
+        return "negativo";
+    }else return "zero";
+}
+module.exports = main;
---
+++ atividade2.js
@@ -0,0 +1,5 @@
+function main(raio) {
+    // codigo
+    return Math.PI*(raio*raio)
+}
+module.exports = main;
